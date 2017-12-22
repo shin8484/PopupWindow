0503fc3691cffe63226951b1e5cc084f3c276a78
hayashi_shinji <hayashi_shinji@cyberagent.co.jp>
2017-12-22 17:23:17 +0900

    initial commit

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Shinji Hayashi on 2017/12/22.
+//  Copyright © 2017年 shin884. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PopupWindow/PopupWindow.h
@@ -0,0 +1,19 @@
+//
+//  PopupWindow.h
+//  PopupWindow
+//
+//  Created by Shinji Hayashi on 2017/12/22.
+//  Copyright © 2017年 shin884. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PopupWindow.
+FOUNDATION_EXPORT double PopupWindowVersionNumber;
+
+//! Project version string for PopupWindow.
+FOUNDATION_EXPORT const unsigned char PopupWindowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PopupWindow/PublicHeader.h>
+
+
